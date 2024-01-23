0f6f62fca74d51bceeb7a1f97956b3db5f3fcdf0
seif eddine rezgui <seifeddine.rezgui@esprit.tn>
2024-01-23 19:59:10 +0100

    initial commit

---
+++ src/main/java/tn/esprit/spring/securecapira/SecurecapiraApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring.securecapira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurecapiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurecapiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/esprit/spring/securecapira/SecurecapiraApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring.securecapira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurecapiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
